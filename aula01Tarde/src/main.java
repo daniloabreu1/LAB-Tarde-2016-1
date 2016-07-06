2ea3ce2a73071efcaa506d05dfde5867041fcc19
Danilo Abreu <daniloabreu1@gmail.com>
2016-07-06 16:56:14 -0300

    editado no site

--- aula01Tarde/src/main.java
+++ aula01Tarde/src/main.java
@@ -15,7 +15,7 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("main.main()");
+        System.out.println("Editado no site");
     }
     
 }
