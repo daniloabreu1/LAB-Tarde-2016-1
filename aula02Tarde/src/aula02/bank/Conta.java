ed6c3d3d704e6816022e14d414e8a5521d4f7d20
daniloabreu1 <daniloabreu1@gmail.com>
2016-07-11 13:48:27 -0300

    aula02.tarde1

---
+++ aula02Tarde/src/aula02/bank/Conta.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02.bank;
+
+/**
+ *
+ * @author Danilo Abreu
+ */
+public class Conta {
+    
+    private int numeroConta;
+    private double saldo;
+    private double limite;
+    private Cliente titular;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public String getTitular() {
+        return titular.nome;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+    
+    
+    
+}
---
+++ aula02Tarde/src/aula02/bank/TesteConta.java
@@ -0,0 +1,14 @@
+package aula02.bank;
+
+/**
+ *
+ * @author Danilo Abreu
+ */
+public class TesteConta {
+    
+    public static void main (String args[]){
+        
+        
+    }
+    
+}
