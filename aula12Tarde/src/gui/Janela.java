559bbad144797c8fe43956b1ba05f24d799a7ddf
daniloabreu1 <daniloabreu1@gmail.com>
2016-09-14 18:00:15 -0300

    gui

---
+++ aula12Tarde/src/gui/Janela.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author Danilo Abreu
+ */
+import javax.swing.*;
+
+public class Janela {
+
+    public Janela() {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+
+    public static void main(String args[]) {
+        new Janela();
+    }
+}
